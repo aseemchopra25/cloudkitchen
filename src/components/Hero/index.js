c4f31510534030d98cc1ec392cef691513b397c0
Aseem Chopra <aseemchopra@protonmail.com>
2021-09-14 19:18:47 +0530

    ✨ finished hero section

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import Navbar from '../Navbar';
-import {HeroContainer} from './HeroElements';
+import {HeroContainer, HeroContent, HeroItems, HeroP, HeroH1, HeroBtn} from './HeroElements';
 const Hero = () => {
     return (
         <HeroContainer>
             <Navbar />
+            <HeroContent>
+                <HeroItems>
+                    <HeroH1>Greatest Pizza Ever</HeroH1>
+                    <HeroP>Ready in 60 seconds</HeroP>
+                    <HeroBtn>Place Order</HeroBtn>
+                </HeroItems>
+            </HeroContent>
         </HeroContainer>
     )
 }
