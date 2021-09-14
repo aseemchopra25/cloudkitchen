2fc142f49e761ce458f70175c72bc149c428b4cc
Aseem Chopra <aseemchopra@protonmail.com>
2021-09-14 18:47:25 +0530

    ✨ added hero section

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import Navbar from "./components/Navbar";
 import React from 'react'
 import {BrowserRouter as Router} from 'react-router-dom';
 import { GlobalStyle } from "./globalStyles";
+import Hero from "./components/Hero";
 
 
 function App() {
   return (
     <Router>
       <GlobalStyle />
-      <Navbar />
+      {/* <Navbar /> already in Hero  */}
+      <Hero />
     </Router>
   );
 }
